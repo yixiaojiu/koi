57f257119ce55a3fca959ab98fc99279dcacbbf3
yixiaojiu <1918418506@qq.com>
2022-09-30 10:20:29 +0800

    feat: :sparkles: init

---
+++ README.md
@@ -0,0 +1,5 @@
+<p align="center">
+    <img alt="logo" src="./src/assets/imgs/logo.png" width="200" height="200" style="margin-bottom: 5px;">
+</p>
+
+<h1 align="center">Koi</h1>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { join } from 'path'
+import Unocss from 'unocss/vite'
+import { presetAttributify, presetIcons, presetUno } from 'unocss'
+
+function resolve(dir: string): string {
+  return join(__dirname, dir)
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  resolve: {
+    alias: {
+      '@': resolve('src/'),
+    },
+  },
+  plugins: [react(),
+    Unocss({
+      presets: [presetUno(), presetIcons(), presetAttributify()],
+    }),
+  ],
 })
